fe3efb9038c16dc63cabc7f4d8e898125f79f03e
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2024-03-22 15:24:41 -0500

    Corrección: Renderizado de tabla resumen de usuario

--- src/components/Table/components/UserSummaryTable.tsx
+++ src/components/Table/components/UserSummaryTable.tsx
@@ -4,7 +4,7 @@ import { useState, useCallback, useEffect } from 'react'
 import usePatients from '../../../hooks/usePatients'
 import { Typography, Table } from 'antd'
 
-export interface PatientWithStretcher extends Omit<PatientData, 'stretcherId'> {
+export interface PatientWithKey extends Omit<PopulatedPatient, 'stretcherId'> {
   key: React.Key
   stretcherId: string | StretcherData | null
 }
@@ -14,36 +14,40 @@ interface UsrSumProps {
 }
 
 export default function UserSummaryTable({ patientId }: UsrSumProps) {
-  const [patient, setPatient] = useState<PatientWithStretcher[] | null>(null)
+  const [patient, setPatient] = useState<PatientWithKey[] | null>(null)
   const [isLoading, setIsLoading] = useState(true)
   const stretchers = useStretchers()
   const patients = usePatients()
 
   const findPatient = useCallback(
     (id: string) => {
-      return patients.find((p) => p._id === id) as PatientWithStretcher
+      const finded = patients.find((p) => p._id === id)
+      if (finded) return JSON.parse(JSON.stringify(finded)) as PatientWithKey 
+      else return null
     },
     [patients]
   )
   const findStretcher = useCallback(
     (id: string) => {
-      return stretchers?.find((s) => s._id === id)
+      const finded = stretchers?.find((s) => s._id === id)
+      if (finded) return JSON.parse(JSON.stringify(finded)) as StretcherData
+      else return null
     },
     [stretchers]
   )
 
   useEffect(() => {
-    if (!patientId) return
-    const patient = findPatient(patientId)
-    if (patient) {
-      if (patient.stretcherId) {
-        const stretcher = findStretcher(patient.stretcherId as string)
-        if (stretcher) patient.stretcherId = stretcher as StretcherData
+    if (!patientId || patient) return
+    const findedPatient = findPatient(patientId)
+    if (findedPatient) {
+      if (findedPatient.stretcherId) {
+        const stretcher = findStretcher(findedPatient.stretcherId as string)
+        if (stretcher) findedPatient.stretcherId = stretcher as StretcherData
       }
-      patient.key = 0
-      setPatient([patient])
+      findedPatient.key = 0
+      setPatient([findedPatient])
     }
-  }, [findPatient, findStretcher, patientId])
+  }, [findPatient, findStretcher, patientId, patient])
 
   useEffect(() => {
     if (patient) setIsLoading(false)
@@ -55,7 +59,7 @@ export default function UserSummaryTable({ patientId }: UsrSumProps) {
         className="CustomTable user-summary"
         loading={isLoading}
         columns={UserSummarySchema}
-        dataSource={patient as PatientWithStretcher[]}
+        dataSource={patient as PatientWithKey[]}
         pagination={{ position: ['none'] }}
         bordered
       />
